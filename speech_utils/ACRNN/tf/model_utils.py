217ddb5d3bb79627fbf0fa9b6bb1284b52632fdf
Hoang Nghia Tuyen <hnt4499@gmail.com>
2020-04-03 15:51:09 +0800

    Beautify

--- speech_utils/ACRNN/tf/model_utils.py
+++ speech_utils/ACRNN/tf/model_utils.py
@@ -13,22 +13,26 @@ def attention(inputs, attention_size, time_major=False, return_alphas=False):
     Note that the author has changed the activation function of the fully
     connected layer from tanh to sigmoid.
 
-    Attention mechanism layer which reduces RNN/Bi-RNN outputs with Attention vector.
+    Attention mechanism layer which reduces RNN/Bi-RNN outputs with Attention
+    vector.
 
-    The idea was proposed in the article by Z. Yang et al., "Hierarchical Attention Networks
-     for Document Classification", 2016: http://www.aclweb.org/anthology/N16-1174.
+    The idea was proposed in the article by Z. Yang et al., "Hierarchical
+    Attention Networks for Document Classification",
+    2016: http://www.aclweb.org/anthology/N16-1174.
     Variables notation is also inherited from the article
 
     Args:
         inputs: The Attention inputs.
             Matches outputs of RNN/Bi-RNN layer (not final state):
                 In case of RNN, this must be RNN outputs `Tensor`:
-                    If time_major == False (default), this must be a tensor of shape:
+                    If time_major == False (default), this must be a tensor of
+                    shape:
                         `[batch_size, max_time, cell.output_size]`.
                     If time_major == True, this must be a tensor of shape:
                         `[max_time, batch_size, cell.output_size]`.
-                In case of Bidirectional RNN, this must be a tuple (outputs_fw, outputs_bw) containing the forward and
-                the backward RNN outputs `Tensor`.
+                In case of Bidirectional RNN, this must be a tuple
+                (outputs_fw, outputs_bw) containing the forward and the backward
+                RNN outputs `Tensor`.
                     If time_major == False (default),
                         outputs_fw is a `Tensor` shaped:
                         `[batch_size, max_time, cell_fw.output_size]`
@@ -41,14 +45,16 @@ def attention(inputs, attention_size, time_major=False, return_alphas=False):
                         `[max_time, batch_size, cell_bw.output_size]`.
         attention_size: Linear size of the Attention weights.
         time_major: The shape format of the `inputs` Tensors.
-            If true, these `Tensors` must be shaped `[max_time, batch_size, depth]`.
-            If false, these `Tensors` must be shaped `[batch_size, max_time, depth]`.
+            If true, these `Tensors` must be shaped
+            `[max_time, batch_size, depth]`.
+            If false, these `Tensors` must be shaped
+            `[batch_size, max_time, depth]`.
             Using `time_major = True` is a bit more efficient because it avoids
-            transposes at the beginning and end of the RNN calculation.  However,
-            most TensorFlow data is batch-major, so by default this function
-            accepts input and emits output in batch-major form.
-        return_alphas: Whether to return attention coefficients variable along with layer's output.
-            Used for visualization purpose.
+            transposes at the beginning and end of the RNN calculation.
+            However, most TensorFlow data is batch-major, so by default this
+            function accepts input and emits output in batch-major form.
+        return_alphas: Whether to return attention coefficients variable along
+            with layer's output. Used for visualization purpose.
     Returns:
         The Attention output `Tensor`.
         In case of RNN, this will be a `Tensor` shaped:
@@ -58,29 +64,32 @@ def attention(inputs, attention_size, time_major=False, return_alphas=False):
     """
 
     if isinstance(inputs, tuple):
-        # In case of Bi-RNN, concatenate the forward and the backward RNN outputs.
+        # In case of Bi-RNN, concatenate the forward and backward RNN outputs.
         inputs = tf.concat(inputs, 2)
 
     if time_major:
         # (T,B,D) => (B,T,D)
         inputs = tf.array_ops.transpose(inputs, [1, 0, 2])
 
-    hidden_size = inputs.shape[2].value  # D value - hidden size of the RNN layer
+    hidden_size = inputs.shape[2].value  # D value - hidden size of RNN layer
 
     # Trainable parameters
-    W_omega = tf.Variable(tf.random_normal([hidden_size, attention_size], stddev=0.1))
+    W_omega = tf.Variable(
+        tf.random_normal([hidden_size, attention_size], stddev=0.1))
     b_omega = tf.Variable(tf.random_normal([attention_size], stddev=0.1))
     u_omega = tf.Variable(tf.random_normal([attention_size], stddev=0.1))
 
-    # Applying fully connected layer with non-linear activation to each of the B*T timestamps;
-    #  the shape of `v` is (B,T,D)*(D,A)=(B,T,A), where A=attention_size
-    # v = tf.tanh(tf.tensordot(inputs, W_omega, axes=1) + b_omega)
+    # Applying fully connected layer with non-linear activation to each of
+    # the B*T timestamps; the shape of `v` is (B,T,D)*(D,A)=(B,T,A),
+    # where A=attention_size
     v = tf.sigmoid(tf.tensordot(inputs, W_omega, axes=1) + b_omega)
-    # For each of the timestamps its vector of size A from `v` is reduced with `u` vector
+    # For each of the timestamps its vector of size A from `v` is reduced with
+    # `u` vector
     vu = tf.tensordot(v, u_omega, axes=1)   # (B,T) shape
     alphas = tf.nn.softmax(vu)              # (B,T) shape also
 
-    # Output of (Bi-)RNN is reduced with attention vector; the result has (B,D) shape
+    # Output of (Bi-)RNN is reduced with attention vector; the result has (B,D)
+    # shape
     output = tf.reduce_sum(inputs * tf.expand_dims(alphas, -1), 1)
 
     if not return_alphas:
@@ -115,8 +124,8 @@ def batch_norm_wrapper(inputs, is_training, decay=0.999, epsilon=1e-3):
 
 
 def acrnn(inputs, num_classes=4, is_training=True, dropout_keep_prob=1,
-          num_filters_1=128, num_filters_2=256, mp_fsize=(2, 4),
-          num_lstm_units=128, num_linear_units=768, num_fcn_units=64):
+          num_filt_1=128, num_filt_2=256, mp_fsize=(2, 4), num_lstm_units=128,
+          num_linear_units=768, num_fcn_units=64):
     """
     Reference: "3-D Convolutional Recurrent Neural Networks with Attention Model
     for Speech Emotion Recognition"
@@ -142,9 +151,9 @@ def acrnn(inputs, num_classes=4, is_training=True, dropout_keep_prob=1,
     dropout_keep_prob : float
         Probability of keeping connections in dropout.
 
-    num_filters_1 : int
+    num_filt_1 : int
         Number of filters of the first convolutional layer.
-    num_filters_2 : int
+    num_filt_2 : int
         Number of filters of all the rest convolutional layers.
     mp_fsize : tuple or list
         Max pooling filter size (tuple/list of height and width, respectively)
@@ -169,50 +178,70 @@ def acrnn(inputs, num_classes=4, is_training=True, dropout_keep_prob=1,
     time_step = image_height // mp_fsize_h
     p = image_width // mp_fsize_w
 
-    layer1_filter = tf.get_variable('layer1_filter', shape=[5, 3, 3, num_filters_1], dtype=tf.float32,
-                                    initializer=tf.truncated_normal_initializer(stddev=0.1))
-    layer1_bias = tf.get_variable('layer1_bias', shape=[num_filters_1], dtype=tf.float32,
-                                  initializer=tf.constant_initializer(0.1))
+    layer1_filter = tf.get_variable(
+        'layer1_filter', shape=[5, 3, 3, num_filt_1], dtype=tf.float32,
+        initializer=tf.truncated_normal_initializer(stddev=0.1))
+    layer1_bias = tf.get_variable(
+        'layer1_bias', shape=[num_filt_1], dtype=tf.float32,
+        initializer=tf.constant_initializer(0.1))
     layer1_stride = [1, 1, 1, 1]
-    layer2_filter = tf.get_variable('layer2_filter', shape=[5, 3, num_filters_1, num_filters_2], dtype=tf.float32,
-                                    initializer=tf.truncated_normal_initializer(stddev=0.1))
-    layer2_bias = tf.get_variable('layer2_bias', shape=[num_filters_2], dtype=tf.float32,
-                                  initializer=tf.constant_initializer(0.1))
+    layer2_filter = tf.get_variable(
+        'layer2_filter', shape=[5, 3, num_filt_1, num_filt_2], dtype=tf.float32,
+        initializer=tf.truncated_normal_initializer(stddev=0.1))
+    layer2_bias = tf.get_variable(
+        'layer2_bias', shape=[num_filt_2], dtype=tf.float32,
+        initializer=tf.constant_initializer(0.1))
     layer2_stride = [1, 1, 1, 1]
-    layer3_filter = tf.get_variable('layer3_filter', shape=[5, 3, num_filters_2, num_filters_2], dtype=tf.float32,
-                                    initializer=tf.truncated_normal_initializer(stddev=0.1))
-    layer3_bias = tf.get_variable('layer3_bias', shape=[num_filters_2], dtype=tf.float32,
-                                  initializer=tf.constant_initializer(0.1))
+    layer3_filter = tf.get_variable(
+        'layer3_filter', shape=[5, 3, num_filt_2, num_filt_2], dtype=tf.float32,
+        initializer=tf.truncated_normal_initializer(stddev=0.1))
+    layer3_bias = tf.get_variable(
+        'layer3_bias', shape=[num_filt_2], dtype=tf.float32,
+        initializer=tf.constant_initializer(0.1))
     layer3_stride = [1, 1, 1, 1]
-    layer4_filter = tf.get_variable('layer4_filter', shape=[5, 3, num_filters_2, num_filters_2], dtype=tf.float32,
-                                    initializer=tf.truncated_normal_initializer(stddev=0.1))
-    layer4_bias = tf.get_variable('layer4_bias', shape=[num_filters_2], dtype=tf.float32,
-                                  initializer=tf.constant_initializer(0.1))
+    layer4_filter = tf.get_variable(
+        'layer4_filter', shape=[5, 3, num_filt_2, num_filt_2], dtype=tf.float32,
+        initializer=tf.truncated_normal_initializer(stddev=0.1))
+    layer4_bias = tf.get_variable(
+        'layer4_bias', shape=[num_filt_2], dtype=tf.float32,
+        initializer=tf.constant_initializer(0.1))
     layer4_stride = [1, 1, 1, 1]
-    layer5_filter = tf.get_variable('layer5_filter', shape=[5, 3, num_filters_2, num_filters_2], dtype=tf.float32,
-                                    initializer=tf.truncated_normal_initializer(stddev=0.1))
-    layer5_bias = tf.get_variable('layer5_bias', shape=[num_filters_2], dtype=tf.float32,
-                                  initializer=tf.constant_initializer(0.1))
+    layer5_filter = tf.get_variable(
+        'layer5_filter', shape=[5, 3, num_filt_2, num_filt_2], dtype=tf.float32,
+        initializer=tf.truncated_normal_initializer(stddev=0.1))
+    layer5_bias = tf.get_variable(
+        'layer5_bias', shape=[num_filt_2], dtype=tf.float32,
+        initializer=tf.constant_initializer(0.1))
     layer5_stride = [1, 1, 1, 1]
-    layer6_filter = tf.get_variable('layer6_filter', shape=[5, 3, num_filters_2, num_filters_2], dtype=tf.float32,
-                                    initializer=tf.truncated_normal_initializer(stddev=0.1))
-    layer6_bias = tf.get_variable('layer6_bias', shape=[num_filters_2], dtype=tf.float32,
-                                  initializer=tf.constant_initializer(0.1))
+    layer6_filter = tf.get_variable(
+        'layer6_filter', shape=[5, 3, num_filt_2, num_filt_2], dtype=tf.float32,
+        initializer=tf.truncated_normal_initializer(stddev=0.1))
+    layer6_bias = tf.get_variable(
+        'layer6_bias', shape=[num_filt_2], dtype=tf.float32,
+        initializer=tf.constant_initializer(0.1))
     layer6_stride = [1, 1, 1, 1]
 
-    linear1_weight = tf.get_variable('linear1_weight', shape=[p * num_filters_2, num_linear_units], dtype=tf.float32,
-                                    initializer=tf.truncated_normal_initializer(stddev=0.1))
-    linear1_bias = tf.get_variable('linear1_bias', shape=[num_linear_units], dtype=tf.float32,
-                                  initializer=tf.constant_initializer(0.1))
+    linear1_weight = tf.get_variable(
+        'linear1_weight', shape=[p * num_filt_2, num_linear_units],
+        dtype=tf.float32, initializer=tf.truncated_normal_initializer(
+            stddev=0.1))
+    linear1_bias = tf.get_variable(
+        'linear1_bias', shape=[num_linear_units], dtype=tf.float32,
+        initializer=tf.constant_initializer(0.1))
 
-    fully1_weight = tf.get_variable('fully1_weight', shape=[2 * num_lstm_units, num_fcn_units], dtype=tf.float32,
-                                    initializer=tf.truncated_normal_initializer(stddev=0.1))
-    fully1_bias = tf.get_variable('fully1_bias', shape=[num_fcn_units], dtype=tf.float32,
-                                  initializer=tf.constant_initializer(0.1))
-    fully2_weight = tf.get_variable('fully2_weight', shape=[num_fcn_units, num_classes], dtype=tf.float32,
-                                    initializer=tf.truncated_normal_initializer(stddev=0.1))
-    fully2_bias = tf.get_variable('fully2_bias', shape=[num_classes], dtype=tf.float32,
-                                  initializer=tf.constant_initializer(0.1))
+    fully1_weight = tf.get_variable(
+        'fully1_weight', shape=[2 * num_lstm_units, num_fcn_units],
+        dtype=tf.float32, initializer=tf.truncated_normal_initializer(
+            stddev=0.1))
+    fully1_bias = tf.get_variable(
+        'fully1_bias', shape=[num_fcn_units], dtype=tf.float32,
+        initializer=tf.constant_initializer(0.1))
+    fully2_weight = tf.get_variable(
+        'fully2_weight', shape=[num_fcn_units, num_classes], dtype=tf.float32,
+        initializer=tf.truncated_normal_initializer(stddev=0.1))
+    fully2_bias = tf.get_variable(
+        'fully2_bias', shape=[num_classes], dtype=tf.float32,
+        initializer=tf.constant_initializer(0.1))
 
     layer1 = tf.nn.conv2d(inputs, layer1_filter, layer1_stride, padding='SAME')
     layer1 = tf.nn.bias_add(layer1, layer1_bias)
@@ -220,35 +249,41 @@ def acrnn(inputs, num_classes=4, is_training=True, dropout_keep_prob=1,
     layer1 = tf.nn.max_pool(
         layer1, ksize=[1, mp_fsize_h, mp_fsize_w, 1],
         strides=[1, mp_fsize_h, mp_fsize_w, 1], padding='VALID', name='max_pool')
-    layer1 = tf.contrib.layers.dropout(layer1, keep_prob=dropout_keep_prob, is_training=is_training)
+    layer1 = tf.contrib.layers.dropout(
+        layer1, keep_prob=dropout_keep_prob, is_training=is_training)
 
     layer2 = tf.nn.conv2d(layer1, layer2_filter, layer2_stride, padding='SAME')
     layer2 = tf.nn.bias_add(layer2, layer2_bias)
     layer2 = leaky_relu(layer2, 0.01)
-    layer2 = tf.contrib.layers.dropout(layer2, keep_prob=dropout_keep_prob, is_training=is_training)
+    layer2 = tf.contrib.layers.dropout(
+        layer2, keep_prob=dropout_keep_prob, is_training=is_training)
 
     layer3 = tf.nn.conv2d(layer2, layer3_filter, layer3_stride, padding='SAME')
     layer3 = tf.nn.bias_add(layer3, layer3_bias)
     layer3 = leaky_relu(layer3, 0.01)
-    layer3 = tf.contrib.layers.dropout(layer3, keep_prob=dropout_keep_prob, is_training=is_training)
+    layer3 = tf.contrib.layers.dropout(
+        layer3, keep_prob=dropout_keep_prob, is_training=is_training)
 
     layer4 = tf.nn.conv2d(layer3, layer4_filter, layer4_stride, padding='SAME')
     layer4 = tf.nn.bias_add(layer4, layer4_bias)
     layer4 = leaky_relu(layer4, 0.01)
-    layer4 = tf.contrib.layers.dropout(layer4, keep_prob=dropout_keep_prob, is_training=is_training)
+    layer4 = tf.contrib.layers.dropout(
+        layer4, keep_prob=dropout_keep_prob, is_training=is_training)
 
     layer5 = tf.nn.conv2d(layer4, layer5_filter, layer5_stride, padding='SAME')
     layer5 = tf.nn.bias_add(layer5, layer5_bias)
     layer5 = leaky_relu(layer5, 0.01)
-    layer5 = tf.contrib.layers.dropout(layer5, keep_prob=dropout_keep_prob, is_training=is_training)
+    layer5 = tf.contrib.layers.dropout(
+        layer5, keep_prob=dropout_keep_prob, is_training=is_training)
 
     layer6 = tf.nn.conv2d(layer5, layer6_filter, layer6_stride, padding='SAME')
     layer6 = tf.nn.bias_add(layer6, layer6_bias)
     layer6 = leaky_relu(layer6, 0.01)
-    layer6 = tf.contrib.layers.dropout(layer6, keep_prob=dropout_keep_prob, is_training=is_training)
+    layer6 = tf.contrib.layers.dropout(
+        layer6, keep_prob=dropout_keep_prob, is_training=is_training)
 
-    layer6 = tf.reshape(layer6,[-1, time_step, num_filters_2 * p])
-    layer6 = tf.reshape(layer6, [-1, p * num_filters_2])
+    layer6 = tf.reshape(layer6, [-1, time_step, num_filt_2 * p])
+    layer6 = tf.reshape(layer6, [-1, p * num_filt_2])
 
     linear1 = tf.matmul(layer6, linear1_weight) + linear1_bias
     linear1 = batch_norm_wrapper(linear1, is_training)
@@ -261,7 +296,8 @@ def acrnn(inputs, num_classes=4, is_training=True, dropout_keep_prob=1,
     # Backward direction cell
     gru_bw_cell1 = tf.contrib.rnn.BasicLSTMCell(num_lstm_units, forget_bias=1.0)
 
-    # Now we feed `layer_3` into the LSTM BRNN cell and obtain the LSTM BRNN output.
+    # Now we feed `layer_3` into the LSTM BRNN cell and obtain the LSTM BRNN
+    # output.
     outputs1, output_states1 = tf.nn.bidirectional_dynamic_rnn(
         cell_fw=gru_fw_cell1, cell_bw=gru_bw_cell1, inputs=linear1,
         dtype=tf.float32, time_major=False, scope='LSTM1')
@@ -339,15 +375,6 @@ def CB_loss_tf(labels, logits, samples_per_cls, beta=0.9999, is_training=True):
     return loss
 
 
-def dense_to_one_hot(labels_dense, num_classes):
-    """Convert class labels from scalars to one-hot vectors."""
-    num_labels = labels_dense.shape[0]
-    index_offset = np.arange(num_labels) * num_classes
-    labels_one_hot = np.zeros((num_labels, num_classes))
-    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
-    return labels_one_hot
-
-
 def train(data, epochs, batch_size, learning_rate, validate_every=10,
           random_seed=123, num_classes=4, grad_clip=False, dropout_keep_prob=1,
           save_path=None, use_CBL=False, beta=0.9999, **kwargs):
@@ -406,7 +433,8 @@ def train(data, epochs, batch_size, learning_rate, validate_every=10,
     keep_prob = tf.placeholder(tf.float32)
     # Cost
     logits = acrnn(
-        X, is_training=is_training, dropout_keep_prob=keep_prob, **kwargs)
+        X, num_classes=num_classes, is_training=is_training,
+        dropout_keep_prob=keep_prob, **kwargs)
     if not use_CBL:
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
             labels=Y, logits=logits)
